8a61cc1294ed425b71819cc563f752ae0f06f8aa
anasofiabeltran <anas.beltran206@gmail.com>
2023-01-26 14:17:17 -0500

    style

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,13 +1,34 @@
 import React from 'react'
 
+import linkedin from '../../Assets/Icons/linkedin.png'
+import github_logo from '../../Assets/Icons/signo-de-github.png'
+
 import './FooterCss.css'
 
 function Footer() {
   return (
     <footer className='Footer'>
       <section className='Footer-content'>
-
-        <p>Basado en el portafolio <a> https://joaotulio.vercel.app/</a></p>
+        <article>
+          <button>
+            <a>
+              <figure>
+                <img  src={linkedin}/>
+              </figure>
+            </a>
+          </button>
+          <button>
+            <a>
+              <figure>
+                <img  src={github_logo}/>
+              </figure>
+            </a>
+          </button>
+          
+          
+        </article>
+        <br />
+        <p>Basado en el portafolio <a href='https://joaotulio.vercel.app/'> https://joaotulio.vercel.app/</a></p>
       </section>
         
     </footer>
